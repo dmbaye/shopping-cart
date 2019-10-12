26085a47e39481e34450500baa8d00d94db60f2b
Djibril M'Baye <djibrilm07@gmail.com>
2019-10-12 11:29:30 +0000

    Removed gulp and added laravel-mix with vue support

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-console.log('App Running...');
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+  el: '#app',
+});
